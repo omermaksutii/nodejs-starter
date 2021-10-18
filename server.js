ad869eecb26b7b7a63422eda8ed464c5aecc6c40
Omer Maksuti <omermaksutiofficial@gmail.com>
2021-10-18 14:38:45 +0200

    sockets updated

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+var cors = require("cors");
+const http = require("http");
+var server = require("http").Server();
+var io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+  },
+});
+app.io = io;
+// io.set("origins", "*:*");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.post("/configurator", (req, res) => {
+  req.app.io.emit("cartevent", { success: "true" });
+  res.send("Hello World!");
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+server.listen(3000, () => {
+  console.log("Listening on 3000.");
+});
